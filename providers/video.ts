a920a8526feb3120f441d996b453c098f6262cf1
yoelrobaina <yoelrobaina96@gmail.com>
2026-04-09 06:02:16 -0400

    Update video.ts

--- providers/video.ts
+++ providers/video.ts
@@ -1,8 +1,8 @@
-// video.ts - Temporalmente deshabilitado para permitir el despliegue
-// TODO: Adaptar a Minimax Video API más adelante
+// video.ts - Deshabilitado temporalmente para permitir despliegue
+// La funcionalidad de video se habilitará más adelante
 
 export async function callVideoProvider(params: any) {
-    throw new Error("Video generation is temporarily disabled. Please use image generation for now.");
+    throw new Error("Video generation is currently disabled. Please use image generation.");
 }
 
 export async function createVideoTask(params: any) {
